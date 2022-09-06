40e5ad0e475103b399d57a0f303bed22d638ef46
Caleb <bambini6@gmail.com>
2022-09-06 13:13:01 -0500

    rearranged structure of functions and event listeners. Cannot figure out how to make my global variables update with the functions as they run.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ var op1 = document.querySelector("#op1")
 var op2 = document.querySelector("#op2")
 var op3 = document.querySelector("#op3")
 var op4 = document.querySelector("#op4")
-var queCount = 0
+ queCount = 0
+
 
 // Setting each questions with it's options and correct option
 var questions = [
@@ -46,54 +47,45 @@ var questions = [
     },
 ];
 
+var que_text = document.querySelector("#question");
+var ques = '<span>' +  questions[queCount].q + '</span>';
+var opt1 = '<button class="option">'+ questions[queCount].ans[0] +'</button>';
+var opt2 = '<button class="option">'+ questions[queCount].ans[1] +'</button>';
+var opt3 = '<button class="option">'+ questions[queCount].ans[2] +'</button>';
+var opt4 = '<button class="option">'+ questions[queCount].ans[3] +'</button>';
 
-
-
-    function showQuestions(queCount) {
-        startbutton.remove()
-        
-
-
-        var que_text = document.querySelector("#question")
-        var ques = "<span>" +  questions[queCount].q + '</span>';
-        var opt1 = '<button class="option">'+ questions[queCount].ans[0] +'</button>'
-        var opt2 = '<button class="option">'+ questions[queCount].ans[1] +'</button>'
-        var opt3 = '<button class="option">'+ questions[queCount].ans[2] +'</button>'
-        var opt4 = '<button class="option">'+ questions[queCount].ans[3] +'</button>';
+function showQuestions() {
+        startbutton.remove();
         que_text.innerHTML = ques; //adding new span tag inside que_tag
         op1.innerHTML = opt1;
         op2.innerHTML = opt2;
         op3.innerHTML = opt3;
         op4.innerHTML = opt4;
+        return;
 
 
         
-                                
-        quizContainer.addEventListener("click", function checkAnswer(event) {
-            var userchoice = event.target.innerHTML;            
-            if (userchoice == questions[queCount].correct) {                
-                console.log(userchoice, "good job!")  
-                                            
-            } 
-            else {
-                console.log("WRONG")
-            }            
-           
-            queCount++
-            console.log(ques)
-        }) 
+    }
+
+function checkAnswer(event) {
+    var userchoice = event.target.innerHTML;            
+    if (userchoice == questions[queCount].correct) {  
+            queCount++;              
+            console.log(userchoice, "good job!") 
+                                        
+        } else {
+            queCount++;
+            console.log("WRONG")
+        }            
+       
+        showQuestions; 
+        console.log(ques);
+        console.log(queCount);
         
 
-        
-        
+        return;
+    }
 
 
-        
-    }   
-
-    
-
-
-
-showQuestions(queCount);
-
+startbutton.addEventListener("click", showQuestions);
+quizContainer.addEventListener("click", checkAnswer); 
