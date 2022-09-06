302d4d07d461f8658f34be16e76357205317d73d
Caleb <bambini6@gmail.com>
2022-09-05 22:55:49 -0500

    added my questions and answers array and began a function tree plan

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+var quizContainer = document.querySelectorAll('.quizcontainer');
+var timer = document.querySelectorAll('.timerbox');
+
+// Setting each questions with it's options and correct option
+var questions = [
+    {
+        q: "This is question 1",
+        ans: [
+            'a',
+            'b',
+            'c',
+            'd'
+        ],
+        correct: 'b',
+    },
+    {
+        q: "This is question 2",
+        ans: [
+            'a',
+            'b',
+            'c',
+            'd'
+        ],
+        correct: 'c',
+    },
+    {   q: "This is question 3",
+        ans: [
+            'a',
+            'b',
+            'c',
+            'd'
+        ],
+        correct: 'a',
+    },
+];
+
+console.log(myQuestions[0].answers);
+
+
+
+function startQuiz(questions, quizcontent) {
+
+
+    function showQuestions(questions, quizContainer, resultsContainer) {
+        // code here
+        var output = [];
+        var answers;
+
+        for ( var i=0; i<questions.length; i++) {
+            answers.push(
+                
+            )
+        }
+    }   
+
+    function showResults(questions, quizcontent, resultsContainer) {
+        // code here
+    }
+
+
+
+    }
+
