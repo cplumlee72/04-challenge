a6b6aab6d216ab244ee82366324eba84f0e1c4fd
Caleb <bambini6@gmail.com>
2022-09-06 21:19:47 -0500

    got the page to respond when clicking through answers by nesting the innerHtml methods inside the checkAnswer function. Now I need to get it to stop when the queCount is done.

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,30 +61,38 @@ function showQuestions() {
         op2.innerHTML = opt2;
         op3.innerHTML = opt3;
         op4.innerHTML = opt4;
+        console.log(ques);
+        console.log(queCount)
         return;
-
-
         
     }
 
 function checkAnswer(event) {
-    var userchoice = event.target.innerHTML;            
-    if (userchoice == questions[queCount].correct) {  
-            queCount++;              
-            console.log(userchoice, "good job!") 
-                                        
-        } else {
-            queCount++;
-            console.log("WRONG")
-        }            
-       
-        showQuestions; 
-        console.log(ques);
+    var userchoice = event.target.innerHTML; 
+    if (queCount < questions.length) {
+        if (userchoice == questions[queCount].correct) {                                      
+            console.log(userchoice, "good job!");                                    
+        } else {                        
+            console.log("WRONG");
+        } 
+        queCount++;
+        var que_text = document.querySelector("#question");
+        var ques = '<span>' +  questions[queCount].q + '</span>';
+        var opt1 = '<button class="option">'+ questions[queCount].ans[0] +'</button>';
+        var opt2 = '<button class="option">'+ questions[queCount].ans[1] +'</button>';
+        var opt3 = '<button class="option">'+ questions[queCount].ans[2] +'</button>';
+        var opt4 = '<button class="option">'+ questions[queCount].ans[3] +'</button>';
+        que_text.innerHTML = ques;
+        op1.innerHTML = opt1;
+        op2.innerHTML = opt2;
+        op3.innerHTML = opt3;
+        op4.innerHTML = opt4;
+        console.log(questions.length);
         console.log(queCount);
         
-
-        return;
-    }
+    } else {
+        console.log("OVER")
+    }};
 
 
 startbutton.addEventListener("click", showQuestions);
